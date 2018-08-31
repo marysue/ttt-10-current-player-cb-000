cd02ae5a42f449d75f8550e4e6b48650b6499c60
Mary Lark <mary@marylark.com>
2018-08-31 19:47:54 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,26 @@
+def turn_count(board)
+  #board = ["O", " ", " ", " ", "X", " ", " ", " ", "X"]
+  #turn_count = 3
+  count = 0
+  board.each | entry | do
+    if entry == 'X' || entry == 'Y'
+      count += 1
+   end
+ end
+ return count
+end
+
+def current_player(board)
+  # board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+  #  returns current player = 'X' for first move
+  #  returns current player = 'Y' for second move
+
+  count = turn_count(board)
+  if count % 2 = 0
+    player = 'Y'
+  else
+    player = 'X'
+  end
+  return player
+end
+
